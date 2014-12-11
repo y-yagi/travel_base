17c4aed35d34bf6a566219dd86e5a358ea695947
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-12-11 09:09:10 +0900

    add user info when create places data

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,9 @@ class Place < ActiveRecord::Base
   include Elasticsearch::Model
 
   class << self
-    def build(params)
+    def build(params, user)
       place = new(params)
+      place.user_id = user.id
       set_geoinfo(place) if params[:latitude].blank?
       place
     end
