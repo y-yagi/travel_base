c1327900b2d3892cb4ffcbc0555522901f85aca6
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-12-23 08:24:38 +0900

    remove unnecessary zip code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
 
   def get_address_form_geocode_result(place)
     # 最初の国名が含まれるので、国名のみ削除する
-    place.formatted_address.split(' ', 2).second
+    # また、郵便番号が含まれる場合、郵便番号も削除
+    place.formatted_address.split(' ', 2).second.gsub(/^〒[0-9-]+ /, '')
   end
 
   def googlemap_link(address)
