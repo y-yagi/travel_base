2fcb72e58b1f3694201125f1916257186e975843
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-01-07 13:06:30 +0900

    clean up Gemfile

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -5,7 +5,6 @@ Geocoder.configure(
   use_https: true,
   api_key: ENV['GOOGLE_MAP_API_KEY'],
   unit: :km,       # :km for kilometers or :mi for miles
-  cache: Redis.new,
   # :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
   # calculation options
   # :distances => :linear    # :spherical or :linear
