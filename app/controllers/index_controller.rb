97542a5e5f189d85505ec230d341cb30a42f8ece
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-02-08 15:11:49 +0900

    fix a variable name having repeated

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,9 +1,9 @@
 class IndexController < ApplicationController
   def index
     @places = Place.mine(current_user).order('created_at DESC')
-    @travels = Travel.schedules.belong(current_user).future
-    gon.events = if @travels.present?
-                      @travels.map do |t|
+    @future_travels = Travel.schedules.belong(current_user).future
+    gon.events = if @future_travels.present?
+                      @future_travels.map do |t|
                         { title: t.name, start: t.start_date, end: t.end_date + 1 }
                       end
                   else
