edcf259bb5c54ca7e2d7c1001afa14b6f0cc7810
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-04-18 08:35:27 +0900

    change the number of search results to the odd number

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @places = Place.search_by(params[:search], current_user, page: params[:page])
+    @places = Place.search_by(params[:search], current_user, page: params[:page], per: 9)
   end
 end
