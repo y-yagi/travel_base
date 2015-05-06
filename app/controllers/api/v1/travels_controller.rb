4805ada07791724a36c04a5b07f5426cc8af74bf
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-05-06 15:04:29 +0900

    change travels order

--- app/controllers/api/v1/travels_controller.rb
+++ app/controllers/api/v1/travels_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::TravelsController < Api::ApplicationController
   include Garage::RestfulActions
 
   def require_resources
-    @resources = Travel.belong(current_resource_owner)
+    @resources = Travel.belong(current_resource_owner).order('start_date DESC')
   end
 
   def require_resource
