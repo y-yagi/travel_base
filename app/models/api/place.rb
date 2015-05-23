03c07dc8bcce2d11efc459bd2aed8931e4701907
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-05-23 10:08:30 +0900

    add address and station info to place api

--- app/models/api/place.rb
+++ app/models/api/place.rb
@@ -5,8 +5,16 @@ module Api::Place
     include Garage::Representer
     property :id
     property :name
+    property :address
     property :memo
     property :latitude
     property :longitude
+    property :station_info
+
+    def station_info
+      places_station.each.sum do |ns|
+        "#{ns.station.line} #{ns.station.name}駅 #{ns.distance}\n"
+      end
+    end
   end
 end
