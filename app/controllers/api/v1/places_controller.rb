b759969e390ef09eeb1d39b43f326759af0d09df
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-11-29 20:08:09 +0900

    can get only updated travels

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::PlacesController < Api::ApplicationController
   include Garage::RestfulActions
 
   def require_resources
-    @resources = ::Place.mine(current_resource_owner).not_gone.order('updated_at DESC')
+    @resources = if params[:updated_at]
+      ::Place.mine(current_resource_owner).includes(places_station: :station).order(updated_at: :desc).where('updated_at > ?', params[:updated_at])
+    else
+      ::Place.mine(current_resource_owner).includes(places_station: :station).order(updated_at: :desc)
+    end
   end
 end
