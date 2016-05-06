e7ff8a93fc7a4590f59f3e53b7ff7dfce6e9e223
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-05-06 18:55:54 +0900

    use `ActionDispatch::IntegrationTest` in deleted data API test

--- test/controllers/api/v1/deleted_data_controller_test.rb
+++ test/controllers/api/v1/deleted_data_controller_test.rb
@@ -1,28 +1,27 @@
 require 'test_helper'
 require 'support/api_helper'
 
-class Api::V1::DeletedDataControllerTest < ActionController::TestCase
+class Api::V1::DeletedDataControllerTest < ActionDispatch::IntegrationTest
   include ApiHelper
 
   setup do
     @user = users(:google)
     @access_token = get_access_token
-    header = 'Bearer ' + @access_token.token
-    @request.env['HTTP_AUTHORIZATION'] = header
+    @authorization = 'Bearer ' + @access_token.token
   end
 
   test 'can get my deleted data list' do
     place = Place.first.destroy!
     travel = Travel.first.destroy!
-    get :index, format: :json, params: { user_id: @user.email, user_provider: @user.provider }
+    get api_v1_deleted_data_path(format: :json), params: { user_id: @user.email, user_provider: @user.provider },
+      headers: { 'HTTP_AUTHORIZATION' => @authorization }
 
     assert_response :success
-    parsed_response_body = JSON.parse(@response.body)
-    assert_equal users(:google).deleted_data.size, parsed_response_body.size
-    assert_equal 'places', parsed_response_body[0]['table_name']
-    assert_equal place.id, parsed_response_body[0]['datum_id']
-    assert_equal 'travels', parsed_response_body[1]['table_name']
-    assert_equal travel.id, parsed_response_body[1]['datum_id']
+    assert_equal users(:google).deleted_data.size, response.parsed_body.size
+    assert_equal 'places', response.parsed_body[0]['table_name']
+    assert_equal place.id, response.parsed_body[0]['datum_id']
+    assert_equal 'travels', response.parsed_body[1]['table_name']
+    assert_equal travel.id, response.parsed_body[1]['datum_id']
   end
 
   test 'specify the update date, the data after the date that update can be get' do
@@ -32,10 +31,11 @@ class Api::V1::DeletedDataControllerTest < ActionController::TestCase
       place.destroy!
     end
 
-    get :index, format: :json, params: { user_id: @user.email, user_provider: @user.provider, updated_at: Time.current + 1 }
+    get api_v1_deleted_data_path(format: :json),
+      params: { user_id: @user.email, user_provider: @user.provider, updated_at: Time.current + 1 },
+      headers: { 'HTTP_AUTHORIZATION' => @authorization }
     assert_response :success
-    parsed_response_body = JSON.parse(@response.body)
-    assert_equal 'places', parsed_response_body[0]['table_name']
-    assert_equal place.id, parsed_response_body[0]['datum_id']
+    assert_equal 'places', response.parsed_body[0]['table_name']
+    assert_equal place.id, response.parsed_body[0]['datum_id']
   end
 end
