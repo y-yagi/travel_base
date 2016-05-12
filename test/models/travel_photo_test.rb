9c2d6a406cc82fb4d30e389b5cf4adbe6f98d423
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-05-12 16:46:09 +0900

    add `active_record_belongs_to_required_by_default` initializer

--- test/models/travel_photo_test.rb
+++ test/models/travel_photo_test.rb
@@ -17,6 +17,6 @@ class TravelPhotoTest < ActiveSupport::TestCase
   test 'should have the necessary required validators' do
     travel_photo = TravelPhoto.new
     assert_not travel_photo.valid?
-    assert_equal [:name, :travel_id, :photo_service_album_id], travel_photo.errors.keys
+    assert_equal [:travel, :photo_service_user_info, :name, :travel_id, :photo_service_album_id], travel_photo.errors.keys
   end
 end
