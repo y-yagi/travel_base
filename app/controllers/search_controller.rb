9559c204b87fd5591fd16b60f4647862b7215a07
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-06-06 08:17:40 +0900

    remove needless branch

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @places = if params[:search].blank?
-      []
-    else
-      current_user.places.ransack({ name_or_address_or_memo_cont: params[:search]}).result
-    end
+    return if params[:search].empty?
+    @places = current_user.places.ransack({ name_or_address_or_memo_cont: params[:search]}).result
   end
 end
