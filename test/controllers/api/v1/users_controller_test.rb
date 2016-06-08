8b014d7e8b0028173f531684468be81d25b8cf78
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-06-08 14:41:11 +0900

    add api for registration token

---
+++ test/controllers/api/v1/users_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+require 'support/api_helper'
+
+class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  include ApiHelper
+
+  setup do
+    @user = users(:google)
+    @access_token = get_access_token
+    @authorization = 'Bearer ' + @access_token.token
+  end
+
+  test 'can update token' do
+    assert_nil @user.device_token
+    patch api_v1_user_path(@user),  params: { user_id: @user.email, user_provider: @user.provider, token: 'token' },
+      headers: { 'HTTP_AUTHORIZATION': @authorization }, as: :json
+
+    assert_response :success
+    assert_equal 'token', @user.reload.raw_device_token
+  end
+end
