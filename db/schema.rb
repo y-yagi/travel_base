272d81bc28424abe1bff9ce5d438e5ab9b254b33
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-07-17 09:33:30 +0900

    add admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608043121) do
+ActiveRecord::Schema.define(version: 20160717003249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -196,6 +195,7 @@ ActiveRecord::Schema.define(version: 20160608043121) do
     t.boolean  "auto_archive", default: false
     t.boolean  "notification", default: false
     t.string   "device_token"
+    t.boolean  "admin",        default: false
   end
 
   add_foreign_key "dropbox_files", "travels"
