045e256c1ba222b67d0b0a7a7637c63fc8ba8508
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-09-02 16:19:18 +0900

    remove needless comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :check_logged_in, :setup
   force_ssl if: :ssl_configured?
@@ -23,7 +21,6 @@ class ApplicationController < ActionController::Base
   def setup
     @travels = Travel.belong(current_user).future
   end
-
 
   def ssl_configured?
     Rails.env.production?
