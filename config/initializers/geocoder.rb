6d892b9d2e6a1ba6dbd088cbe0c5d435b1fb34f4
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-09-02 16:40:26 +0900

    remove comments

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -4,8 +4,5 @@ Geocoder.configure(
   language: :ja,
   use_https: true,
   api_key: ENV['GOOGLE_MAP_API_KEY'],
-  unit: :km,       # :km for kilometers or :mi for miles
-  # :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
-  # calculation options
-  # :distances => :linear    # :spherical or :linear
+  unit: :km
 )
