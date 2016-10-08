a2537864716848b46737531732f112ac0ca4634b
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-10-08 17:29:43 +0900

    remove unrelated files

--- test/mailers/previews/foo_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/foo_mailer
-class FooMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/foo_mailer/user
-  def user
-    FooMailer.user
-  end
-
-end
