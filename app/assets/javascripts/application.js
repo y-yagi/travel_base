cb5260dcfaf5cdd137131487214c5903d9ec3709
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-01-19 09:46:11 +0900

    fix file path

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-css
+//= require bootstrap/dist/js/bootstrap.min
 //= require bootstrap-tagsinput
 //= require moment/min/moment.min
 //= require moment/locale/ja
