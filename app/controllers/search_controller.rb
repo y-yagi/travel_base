4cd185baa604fb81d7afdc7660c9f294c4f0c96d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-10-23 12:51:54 +0900

    Do not use `ransack`

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     return if params[:search].empty?
-    @places = current_user.places.ransack({ name_or_address_or_memo_cont: params[:search]}).result
+    @places = current_user.places.search(params[:search])
   end
 end
