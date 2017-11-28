1ad9c49ffe2285a56455ddcecaaac6e06355788f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-29 07:51:25 +0900

    specify screen size

--- test/system/search_test.rb
+++ test/system/search_test.rb
@@ -1,7 +1,7 @@
 require 'application_system_test_case'
 
 class SearchTest < ApplicationSystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [500, 500]
 
   setup do
     login
