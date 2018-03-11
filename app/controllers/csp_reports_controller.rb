3337af15675e78e8bd9a93e7736766ac09acc6c8
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-03-12 08:11:57 +0900

    specify mime type

--- app/controllers/csp_reports_controller.rb
+++ app/controllers/csp_reports_controller.rb
@@ -2,7 +2,7 @@ class CspReportsController < ActionController::Base
   skip_forgery_protection
 
   def create
-    csp = JSON.parse(request.body.read)["csp-report"]
+    Rails.logger.info(params["csp-report"])
     head :ok
   end
 end
