44853191048fd1a3abac1bdf3f1aeb6fb4e75bc6
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-03-12 08:22:06 +0900

    add google analytics

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,7 +9,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, :data, "fonts.gstatic.com", "netdna.bootstrapcdn.com"
   policy.img_src     :self, :data
   policy.object_src  :none
-  policy.script_src  :self, "www.dropbox.com", "maps.googleapis.com", :unsafe_inline
+  policy.script_src  :self, "www.dropbox.com", "maps.googleapis.com", "www.google-analytics.com", :unsafe_inline
   policy.style_src   :self, "netdna.bootstrapcdn.com", "fonts.googleapis.com", :unsafe_inline
 
   # Specify URI for violation reports
